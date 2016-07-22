ca2b340e1b8b053d85ac9d976bc9f05ddce01c75
aringhieri.stefano <aringhieri.stefano@gmail.com>
2016-07-22 07:22:56 -0700

    init folder

---
+++ Firmware/prova_gripper_v0/prova_gripper_v0.ino
@@ -0,0 +1,20 @@
+
+#include "MeOrion.h"
+#include <Wire.h>
+#include <SoftwareSerial.h>
+MeDCMotor motor1(PORT_1);
+uint8_t motorSpeed = 200;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  motor1.run(motorSpeed);
+  delay(1000);
+  motor1.run(-motorSpeed);
+  delay(1000);
+
+  
+}
